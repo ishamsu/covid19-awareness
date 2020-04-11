e84c6c8e626b55c21df73592d9d1b97079d94f49
ishamsu <shamsu1997@gmail.com>
2020-04-12 00:40:47 +0530

    added community-kitchen.html & communitykitchen.js

--- docs/assets/js/whatsnew.js
+++ docs/assets/js/whatsnew.js
@@ -3,21 +3,20 @@ var whatsnew = {
     "Quarantine Vault by @makersofkerala": "https://makers-of-kerala.now.sh/quarantine-vault",
     "List of hackathons and price offering for solving different COVID-19 problem space": "https://makergram.com/community/topic/124/list-of-hackathons-and-price-offering-for-solving-different-covid-19-problem-space",
     "Automated Dino game using arduino": "https://makergram.com/blog/automated-dino-game-using-arduino/"
-  }
-  var data = JSON.stringify(whatsnew);
-  var obj = JSON.parse(data);
-  var whatsNewContant = Object.keys(obj);
-  let i = 0;
-  var button, content
+}
+var data = JSON.stringify(whatsnew);
+var obj = JSON.parse(data);
+var whatsNewContant = Object.keys(obj);
+var i = 0;
+var button, content
 
-  //function which will run every 5 sec
-  setInterval(function () {
+//function which will run every 5 sec
+setInterval(function() {
     content = `<h3 style="color:black;"><a  href="${obj[whatsNewContant[i]]}">${whatsNewContant[i]}</a></h3>`;
-   
+
     i++;
     if (i == whatsNewContant.length) {
-      i = 0;
+        i = 0;
     }
     document.getElementById("whatsNewContant").innerHTML = content;
-  }, 5000);
-
+}, 5000);
